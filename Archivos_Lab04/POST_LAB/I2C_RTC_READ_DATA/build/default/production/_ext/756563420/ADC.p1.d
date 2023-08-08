b6dd3e540636f7f82464518104f768bcfd4b0fe6
AleG13 <115447303+AleG13@users.noreply.github.com>
2023-08-08 12:26:06 -0600

    Add files via upload

---
+++ Archivos_Lab04/POST_LAB/I2C_RTC_READ_DATA/build/default/production/_ext/756563420/ADC.p1.d
@@ -0,0 +1,3 @@
+build/default/production/_ext/756563420/ADC.p1:  \
+C:/Users/Alejandro\ Gonzalez/OneDrive\ -\ Universidad\ del\ Valle\ de\ Guatemala/Entregables/Digital\ 2/ARCHIVOS_LAB4/POST_LAB/I2C_RTC_READ_DATA/ADC.c  \
+C:/Users/Alejandro\ Gonzalez/OneDrive\ -\ Universidad\ del\ Valle\ de\ Guatemala/Entregables/Digital\ 2/ARCHIVOS_LAB4/POST_LAB/I2C_RTC_READ_DATA/ADC.h 
---
+++ Archivos_Lab04/POST_LAB/I2C_RTC_READ_DATA/build/default/production/_ext/756563420/LCD.p1.d
@@ -0,0 +1,3 @@
+build/default/production/_ext/756563420/LCD.p1:  \
+C:/Users/Alejandro\ Gonzalez/OneDrive\ -\ Universidad\ del\ Valle\ de\ Guatemala/Entregables/Digital\ 2/ARCHIVOS_LAB4/POST_LAB/I2C_RTC_READ_DATA/LCD.c  \
+C:/Users/Alejandro\ Gonzalez/OneDrive\ -\ Universidad\ del\ Valle\ de\ Guatemala/Entregables/Digital\ 2/ARCHIVOS_LAB4/POST_LAB/I2C_RTC_READ_DATA/LCD.h 
