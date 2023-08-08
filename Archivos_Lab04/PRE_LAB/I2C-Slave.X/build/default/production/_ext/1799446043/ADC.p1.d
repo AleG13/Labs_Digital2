26975b3e5dd81e475323c701b703c4b9b17d6f61
AleG13 <115447303+AleG13@users.noreply.github.com>
2023-08-08 12:22:00 -0600

    Add files via upload

---
+++ Archivos_Lab04/PRE_LAB/I2C-Slave.X/build/default/production/_ext/1799446043/ADC.p1.d
@@ -0,0 +1,3 @@
+build/default/production/_ext/1799446043/ADC.p1:  \
+C:/Users/Alejandro\ Gonzalez/OneDrive\ -\ Universidad\ del\ Valle\ de\ Guatemala/Entregables/Digital\ 2/ARCHIVOS_LAB4/PRE_LAB/I2C-Slave.X/ADC.c  \
+C:/Users/Alejandro\ Gonzalez/OneDrive\ -\ Universidad\ del\ Valle\ de\ Guatemala/Entregables/Digital\ 2/ARCHIVOS_LAB4/PRE_LAB/I2C-Slave.X/ADC.h 
