4b8f2251f4bc50a1e1bb5ccd1cb66bf896f6411e
AleG13 <115447303+AleG13@users.noreply.github.com>
2023-09-21 22:13:04 -0600

    Update PostLab.ino

--- Archivos_Lab06/PostLab/PostLab/PostLab.ino
+++ Archivos_Lab06/PostLab/PostLab/PostLab.ino
@@ -1,22 +1,3 @@
-/*
-  SD card read/write
-
- This example shows how to read and write data to and from an SD card file
- The circuit:
- * SD card attached to SPI bus as follows:
- ** MOSI - pin 11
- ** MISO - pin 12
- ** CLK - pin 13
- ** CS - pin 4
-
- created   Nov 2010
- by David A. Mellis
- modified 9 Apr 2012
- by Tom Igoe
-
- This example code is in the public domain.
-
- */
 
 #include <SPI.h>
 #include <SD.h>
@@ -37,10 +18,7 @@ void setup()
   SPI.setModule(0);
   
   Serial.print("Initializing SD card...");
-  // On the Ethernet Shield, CS is pin 4. It's set as an output by default.
-  // Note that even if it's not used as the CS pin, the hardware SS pin
-  // (10 on most Arduino boards, 53 on the Mega) must be left as an output
-  // or the SD library functions will not work.
+
   pinMode(12, OUTPUT);
 
   if (!SD.begin(12)) {
